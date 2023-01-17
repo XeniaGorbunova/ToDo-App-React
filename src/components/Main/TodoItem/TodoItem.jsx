a6002e77c4641c8367f0889375238e313b5ac4bc
XeniaGorbunova <GorbunovaKsenia@gmail.com>
2023-01-17 18:21:00 +0300

    improve mobile view

--- src/components/Main/TodoItem/TodoItem.jsx
+++ src/components/Main/TodoItem/TodoItem.jsx
@@ -54,10 +54,10 @@ function TodoItem({
           .
           {' '}
         </span>
-        <span className={`${completed ? styles.done : ''} ${isEditing ? 'd-none' : 'visible'}`}>
+        <p className={`mb-0 ${completed ? styles.done : ''} ${isEditing ? 'd-none' : 'visible'}`}>
           {title}
-        </span>
-        <form className={`d-flex justify-content-between align-items-center ${isEditing ? 'visible' : 'invisible'}`} onSubmit={editHandler}>
+        </p>
+        <form className={`d-flex justify-content-between align-items-center ${isEditing ? 'visible' : 'd-none'}`} onSubmit={editHandler}>
           <div>
             <label htmlFor={id}>
               New name for
@@ -76,7 +76,7 @@ function TodoItem({
           <div>
             <button
               type="button"
-              className="btn btn-dark mx-2"
+              className="btn btn-dark mx-2 mb-1"
               onClick={() => setEditing(false)}
             >
               Cancel
@@ -107,16 +107,18 @@ function TodoItem({
         <button
           onClick={completeHandler}
           type="button"
-          className={`btn btn-${completed ? 'primary' : 'success'} mx-2 ${isEditing ? 'd-none' : 'visible'}`}
+          className={`btn mb-1 btn-${completed ? 'primary' : 'success'} mx-2 ${isEditing ? 'd-none' : 'visible'}`}
         >
           {completed ? 'Undone' : 'Done'}
         </button>
-        <button onClick={openDeleteModalHandler} type="button" className="btn btn-danger mx-2">Delete</button>
+        <button onClick={openDeleteModalHandler} type="button" className="btn btn-danger mx-2 mb-1">
+          Delete
+        </button>
         <button
           onClick={() => setEditing(true)}
           ref={editButtonRef}
           type="button"
-          className={`btn btn-primary mx-2 ${isEditing ? 'd-none' : 'visible'}`}
+          className={`btn mx-2 mb-1 btn-primary ${isEditing ? 'd-none' : 'visible'}`}
         >
           Edit
         </button>
